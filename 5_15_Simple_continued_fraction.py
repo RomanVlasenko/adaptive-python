edc34d3b1550e8c6521d2b55da815a53a19e65e0
Roman Vlasenko <ipcreeper@Romans-MacBook-Pro.local>
2018-01-28 20:46:14 +0100

    5.15 Simple continued fraction

--- 5_15_Simple_continued_fraction.py
+++ 5_15_Simple_continued_fraction.py
@@ -1,13 +1,15 @@
 s = input()
 
 
-def solve(a, b, koefs):
-    (c, d) = divmod(a, b)  # (7, 29)
-    koefs.append(c)
+#  239/30
+def solve(a, d, koefs):  # (239/30) -> (30/29) -> (29/1)
+    (n, r) = divmod(a, d)  # (7, 29) -> (1, 1) -> (29, 0)
+    koefs.append(n)  # 7 -> 1 -> 29
 
-    if d > 0:
-        solve(b, d, koefs)
+    if r > 0:
+        solve(d, r, koefs)
 
     return koefs
+
 
 print(" ".join([str(k) for k in solve(int(s.split("/")[0]), int(s.split("/")[1]), [])]))
