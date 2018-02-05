fdcb072668e5217cc68ef996f41eb1285362af2e
Roman Vlasenko <ipcreeper@Romans-MacBook-Pro.local>
2018-02-05 19:46:20 +0100

    FizzBuzz solution

---
+++ Fizz_Buzz.py
@@ -0,0 +1,11 @@
+a, b = map(int, input().split())
+
+for n in range(a, b + 1):
+    if n % 3 == 0 and n % 5 == 0:
+        print("FizzBuzz")
+    elif n % 3 == 0:
+        print("Fizz")
+    elif n % 5 == 0:
+        print("Buzz")
+    else:
+        print(n)
